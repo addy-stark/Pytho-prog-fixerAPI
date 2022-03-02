826231d8b53eeb69fdb5731c70c8e0a784dcb8cf
Abhishek Sabharwal <67104587+addy-stark@users.noreply.github.com>
2022-03-02 08:38:18 +0000

    Update fixerAPIcurrency.py

--- fixerAPIcurrency.py
+++ fixerAPIcurrency.py
@@ -1,18 +1,18 @@
 import requests
 import time
-money = 100
+money = 100 #GBP
 url = "http://data.fixer.io/api/convert?access_key=0d27dd8ffbcfacd37de7f1e7111000bf&from=USD&to=GBP&amount=money"
-res = requests.get(url)
-result = ''
-while result != True:
-    try:
-        res = requests.get(url)
-        result = res.json()['success']
-        if result == False:
-            print('fail' + ' -access restricted for this plan')
-        else:
-            print(res.json()['info']['rate'])
-            print('success')
-        time.sleep(3)
-    except:
-        pass
+result = False
+i = 1
+while result != True and i < 20:  
+    res = requests.get(url)
+    result = res.json()['success']
+    if result == False:
+        print(str(result) + ' -access restricted for this plan, ' + 'try ' + str(i))
+        i+=1
+        if i==20:
+            print('API not providing conversion value. Please check your internet or your fixer plan and try again.')
+    else:
+        print('Current conversion rate ' + res.json()['info']['rate']) ##conversion to usd
+    time.sleep(3)
+
